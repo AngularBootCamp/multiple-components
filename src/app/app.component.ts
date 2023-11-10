1f98ec590d49b626ede4a738f9d7974554e1dffe
Lance Finney <lance.finney@oasisdigital.com>
2023-11-10 11:12:30 -0600

    curriculum update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { HrFilesSearchComponent } from './hr-files/hr-files-search.component';
+import { PayrollSearchComponent } from './payroll/payroll-search.component';
+
 @Component({
   selector: 'app-root',
   template: `
     <app-payroll-search></app-payroll-search>
     <app-hr-files-search></app-hr-files-search>
-  `
+  `,
+  standalone: true,
+  imports: [PayrollSearchComponent, HrFilesSearchComponent]
 })
 export class AppComponent {}
